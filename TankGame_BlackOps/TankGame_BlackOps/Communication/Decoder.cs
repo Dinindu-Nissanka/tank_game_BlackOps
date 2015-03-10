f2d858bafe08902f971e7f8dc120591dea28deac
Dinindu-Nissanka <dmmnissanka@gmail.com>
2015-03-10 17:43:33 +0530

    Communicator class

---
+++ TankGame_BlackOps/TankGame_BlackOps/Communication/Decoder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TankGame_BlackOps.Communication
+{
+    class Decoder
+    {
+        private string[] initMessage;
+        public void Decode_message(string message){
+            initMessage = Regex.Split(message, ":");
+            switch(initMessage[0]){
+                case "I":
+                    break;
+                case "C"
+
+            }
+        }
+    }
+}
